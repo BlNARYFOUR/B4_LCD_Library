705fd37baf0f2226cec46453f0084730499f1362
Brend Lambert <lambertbrend@gmail.com>
2020-01-09 20:08:26 +0100

    better, bug fix

--- src/B4_LCD.cpp
+++ src/B4_LCD.cpp
@@ -367,6 +367,15 @@ void B4Lcd::font(uint16_t num, uint16_t maxDigits, uint16_t col, uint16_t row) {
 	uint16_t centi = 1;
 	uint16_t digits = 0;
 	
+	if(num == 0) {
+		do {
+			font((byte)0, col, row);
+			col += 5;
+		} while (0 < --maxDigits);
+		
+		return;
+	}
+	
 	while (0 < num / centi) {
 		centi *= 10;
 		digits++;
